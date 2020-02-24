8deaae46ab6b283dd2d35aa7aa6ab37b45e565af
selmihamza <selmi.hamza@gmail.com>
2020-02-24 17:09:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Profile from "./profile/profile";
+import "./App.css";
 
 function App() {
+  let data = {
+    fullname: "foulen ben foulen",
+    bio: "bla bla bla",
+    profession: "Teacher"
+  };
+
+  const handleName = e => {
+    // e.preventDefault();
+    alert(data.fullname);
+  };
+
+  const style = {
+    width: "500px",
+    height: "600px",
+    borderRadius: "10px",
+    border: "1px solid black",
+    marginTop: "10px"
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile data={data}>
+        <img
+          onClick={handleName}
+          style={style}
+          alt="img"
+          src="https://i.pinimg.com/originals/c3/e5/98/c3e598ad24447e2a25e805a9a3b76315.jpg"
+        />
+      </Profile>
     </div>
   );
 }
